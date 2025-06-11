88aff6b47b3e9247c300060101911989f4f24c47
Jiho <heyziizi@gmail.com>
2025-06-11 11:14:42 +0900

    Upload doodi

---
+++ android/app/src/main/kotlin/com/example/doodi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.doodi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screen/NewPost.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewPost extends StatefulWidget {
+  const NewPost({super.key});
+
+  @override
+  State<NewPost> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<NewPost> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
