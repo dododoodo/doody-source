3b9cf5a0efb15a592b934042c44bfb1e57ae3548
Doyeon Kim <nsa10050@naver.com>
2025-06-12 11:10:03 +0900

    do

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blackColor = Color(0xFF121212);
+  static const Color whiteColor = Color(0xFFFBFBFB);
+  static const Color darkModeColor = Color(0xFF1E1E1E);
+  static const Color greyColor = Color(0xFF5C5C5C);
+  static const Color lineColor = Color(0xFFE5E7EB);
+  static const Color pointBlue = Color(0xFF2227F7);
+  static const Color pointYellow = Color(0xFFFBE62A);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:doodi/screen/Home.dart';
+import 'package:doodi/screen/NewPost.dart';
+import 'package:doodi/screen/Settings.dart';
 import 'package:flutter/material.dart';
+import 'package:doodi/constants/colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,58 +15,132 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'doodi',
+      title: 'doody',
+
+      // Light Theme (default)
       theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.whiteColor,
+        appBarTheme: AppBarTheme(backgroundColor: AppColors.whiteColor),
+        bottomAppBarTheme: BottomAppBarTheme(color: AppColors.whiteColor),
+        iconTheme: IconThemeData(color: AppColors.blackColor),
         fontFamily: 'free-4',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: AppColors.blackColor),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.blackColor,
+          brightness: Brightness.light,
+        ),
+        cardColor: Color(0xFFFFFFFF),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      // Dark Theme
+      darkTheme: ThemeData(
+        scaffoldBackgroundColor: AppColors.blackColor,
+        appBarTheme: AppBarTheme(backgroundColor: AppColors.blackColor),
+        bottomAppBarTheme: BottomAppBarTheme(color: AppColors.blackColor),
+        iconTheme: IconThemeData(color: AppColors.whiteColor),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: AppColors.whiteColor),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.whiteColor,
+          brightness: Brightness.dark,
+        ),
+        cardColor: Color(0xFF1E1E1E),
+      ),
+
+      themeMode: ThemeMode.system,
+      home: const MyHomePage(title: '두디-Doody'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int pageIndex = 0;
+  int navIndex = 0;
 
-  void _incrementCounter() {
+  void changeIndex(int idx) {
     setState(() {
-      _counter++;
+      pageIndex = idx;
     });
   }
+
+  List<Widget> get pageList => [Home(), Settings()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        toolbarHeight: 65,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SvgPicture.asset('assets/imgs/main_logo_01.svg'),
+              SvgPicture.asset('assets/imgs/main_logo_02.svg'),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      body: pageList[pageIndex],
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: navIndex,
+
+        selectedItemColor: Theme.of(context).iconTheme.color,
+        unselectedItemColor: Theme.of(context).brightness == Brightness.dark
+            ? const Color.fromRGBO(255, 255, 255, 0.5)
+            : const Color.fromRGBO(18, 18, 18, 0.5),
+
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NewPost()),
+            );
+          } else {
+            setState(() {
+              navIndex = index;
+              pageIndex = index == 0 ? 0 : 1;
+            });
+          }
+        },
+
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home, size: 27), label: '홈'),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.all(6),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColors.pointBlue,
+                ),
+                padding: const EdgeInsets.all(16),
+                child: Icon(Icons.add, color: Colors.white),
+              ),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings, size: 27),
+            label: '설정',
+          ),
+        ],
+      ),
     );
   }
 }
